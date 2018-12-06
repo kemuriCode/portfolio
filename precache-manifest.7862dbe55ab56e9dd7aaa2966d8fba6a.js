94d2feaf9ee14031b7d98dda4532cf03061dac6d
t0shifollow <marcindymek24@gmail.com>
2018-12-06 15:57:45 +0100

    Updates

---
+++ precache-manifest.7862dbe55ab56e9dd7aaa2966d8fba6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dc5428eb6480827ff0bfcfaf37a1c9b",
+    "url": "/portfolio/static/media/avatar.0dc5428e.png"
+  },
+  {
+    "revision": "e000ff5d1ef894324d79d783aeca293c",
+    "url": "/portfolio/static/media/avatar-menu.e000ff5d.png"
+  },
+  {
+    "revision": "58e4aa8b8d9c8a5be7494c7541c859c8",
+    "url": "/portfolio/static/media/avatar-contact.58e4aa8b.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4ef1c4695bd363f22500",
+    "url": "/portfolio/static/js/main.4ef1c469.chunk.js"
+  },
+  {
+    "revision": "c33ba76a5fc35ebb2596",
+    "url": "/portfolio/static/js/1.c33ba76a.chunk.js"
+  },
+  {
+    "revision": "4ef1c4695bd363f22500",
+    "url": "/portfolio/static/css/main.7191e732.chunk.css"
+  },
+  {
+    "revision": "c33ba76a5fc35ebb2596",
+    "url": "/portfolio/static/css/1.77d56538.chunk.css"
+  },
+  {
+    "revision": "a1d08b57a5579ee0f07b55f1dba1f858",
+    "url": "/portfolio/index.html"
+  }
+];

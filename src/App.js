146ce4b9aa11f202e2d76f84b29ae9c4c93a930a
t0shifollow <marcindymek24@gmail.com>
2018-12-06 16:09:39 +0100

    add new path

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ class App extends Component {
     return (
       <div className="demo-big-content">
           <Layout>
-              <Header className="header" title={<Navigation className="logo-name"><Link to="/">MARCIN DYMEK</Link></Navigation>} scroll>
+              <Header className="header" title={<Navigation className="logo-name"><Link to={`${process.env.PUBLIC_URL}/`}>MARCIN DYMEK</Link></Navigation>} scroll>
                   <Navigation>
-                    <Link to ="/resume">RESUEM</Link>
-                    <Link to ="/aboutme">ABOUT ME</Link>
-                    <Link to ="/projects">PROJECTS</Link>
-                    <Link to ="/contact">CONTACT</Link>
+                    <Link to ={`${process.env.PUBLIC_URL}/resume`}>RESUEM</Link>
+                    <Link to={`${process.env.PUBLIC_URL}/aboutme`}>ABOUT ME</Link>
+                    <Link to ={`${process.env.PUBLIC_URL}/projects`}>PROJECTS</Link>
+                    <Link to ={`${process.env.PUBLIC_URL}/contact`}>CONTACT</Link>
                   </Navigation>
               </Header>
               <Drawer className="slide-menu" title="MARCIN DYMEK">
                   <Navigation>
-                    <Link to="/"><img className="menu-logo" src={require('./image/avatar-menu.png')}/></Link>
-                    <Link to ="/resume">RESUME</Link>
-                    <Link to ="/aboutme">ABOUT ME</Link>
-                    <Link to ="/projects">PROJECTS</Link>
-                    <Link to ="/contact">CONTACT</Link>
+                    <Link to={`${process.env.PUBLIC_URL}/`}><img className="menu-logo" src={require('./image/avatar-menu.png')}/></Link>
+                    <Link to ={`${process.env.PUBLIC_URL}/resume`}>RESUEM</Link>
+                    <Link to={`${process.env.PUBLIC_URL}/aboutme`}>ABOUT ME</Link>
+                    <Link to ={`${process.env.PUBLIC_URL}/projects`}>PROJECTS</Link>
+                    <Link to ={`${process.env.PUBLIC_URL}/contact`}>CONTACT</Link>
                   </Navigation>
               </Drawer>
               <Content>
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,11 +10,11 @@ import Resume from './Resume';
 const Main = () => (
     // eslint-disable-next-line no-unused-expressions
     <Switch>
-        <Route exact path ={process.env.PUBLIC_URL + '/'} component={LandingPage} />
-        <Route path ={process.env.PUBLIC_URL + '/aboutme'} component={AboutMe} />
-        <Route path ={process.env.PUBLIC_URL + '/contact'} component={Contact} />
-        <Route path ={process.env.PUBLIC_URL + '/projects'} component={Projects} />
-        <Route path ={process.env.PUBLIC_URL + '/resume'} component={Resume} />
+        <Route exact path ={`${process.env.PUBLIC_URL}/`} component={LandingPage} />
+        <Route path ={`${process.env.PUBLIC_URL}/aboutme`} component={AboutMe} />
+        <Route path ={`${process.env.PUBLIC_URL}/contact`} component={Contact} />
+        <Route path ={`${process.env.PUBLIC_URL}/project`} component={Projects} />
+        <Route path ={`${process.env.PUBLIC_URL}/resume`} component={Resume} />
     </Switch>
 );
 
